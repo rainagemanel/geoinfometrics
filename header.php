ca55498fda170668921a450202eb58bed0f75cde
1Laxuz <102129882+1Laxuz@users.noreply.github.com>
2024-03-14 09:51:41 +0800

    modified:   header.php

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Responsive Minimalistic Navbar with Improved Hover Animation</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <link href="styleheader.css" rel="stylesheet" type="text/css">
-</head>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Minimalistic Navbar with Improved Hover Animation</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link href="styleheader.css" rel="stylesheet" type="text/css">
+  </head>
 <body>
 
 
