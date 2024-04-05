e868cf204bd290f1f2eab6c250ad9c839f30a1cc
bvn05 <kfce05@gmail.com>
2024-04-05 13:20:51 +0800

    new file:   dist/css/services.css 	new file:   dist/img/abg.jpg 	new file:   dist/img/bg1.jpg 	new file:   dist/img/geo.jpg  new file:   dist/img/lpp.jpg 	new file:   dist/img/mis.jpg 	new file:   pages/geo.php 	new file:   pages/main.js 	new file:   pages/management.php 	new file:   pages/planning.php

---
+++ pages/management.php
@@ -0,0 +1,217 @@
+
+<?php include 'header.php'; ?>
+</body>
+</nav>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Management Information System</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../dist/css/services.css">
+   
+</head>
+<body>
+<header class="LOGO">
+      <!-- <h1>GeoInfometrics Solutions Corp <img class="log" src="as1.png"></h1>
+
+          <nav class="die">
+            <a href="#">Servicesssssss</a>
+            <a href="#">Project</a>
+            <a href="#">About Us</a>
+          </nav> -->
+    </header>
+
+    <section id="msp" class="msp">
+<div class="tit" style="text-align: center; color: black;">
+  <!-- <h1>
+  "Good fortune is what happens 
+</h1>
+<h1>
+when opportunity meets with
+</h1>
+<h1>
+  <b>Planning."</b>
+</h1>
+<button type="button">Let's get Started!</button>
+</div> -->
+</section>
+
+<!-- SERVICES TO CHOOSE CODE -->
+<section id="choices" class="choices">
+  <div class="column" onmouseover="enlargeColumn(this)" onmouseout="resetColumn(this)">
+    <div class="card" style="width: 20rem;">
+      <img src="../dist/img/bg1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="text-align: center; font-weight: 900;">Environmental Planning Services</h5>
+        <ul style="font-size: 11px; letter-spacing: 0px;">
+          <li>Climate and Disaster Risk Assessment</li>
+          <li>Disaster Risk Reduction and Management Plan</li>
+          <li>Ecological Profile</li>
+          <li>Initial Environmental Examination /Environmental Impact Assessment</li>
+          <li>Solid Waste Management Plan</li>
+        </ul>
+        <button class="button1" id="navbtn" style="font-size: 12px;">Learn More</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="column" onmouseover="enlargeColumn(this)" onmouseout="resetColumn(this)">
+    <div class="card" style="width: 20rem;">
+      <img src="../dist/img/bg1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="text-align: center; font-weight: 900;">Land Use Planning</h5>
+        <ul style="font-size: 11px; letter-spacing: 0px;">
+          <li>Cave Management Plan</li>
+          <li>Comprehensive Development Plan</li>
+          <li>Comprehensive Land Use Plan</li>
+          <li>Forest Land Use Plan</li>
+          <li>Tourism Master Plan</li>
+        </ul>
+        <button class="button1" id="navbtn1" style="font-size: 12px;">Learn More</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="column" onmouseover="enlargeColumn(this)" onmouseout="resetColumn(this)">
+    <div class="card" style="width: 20rem;">
+      <img src="../dist/img/bg1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="text-align: center; font-weight: 900;">Community Development Planning</h5>
+        <ul style="font-size: 11px; letter-spacing: 0px;">
+          <li>Gender and Development Plan</li>
+          <li>Local Climate Change Action Plan</li>
+          <li>Local Shelter Plan</li>
+          <li>Other government mandated plans</li>
+        </ul>
+        <button class="button1" id="navbtn2" style="font-size: 12px;">Learn More</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="column" onmouseover="enlargeColumn(this)" onmouseout="resetColumn(this)">
+    <div class="card" style="width: 20rem;">
+      <img src="../dist/img/bg1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="text-align: center; font-weight: 900;">Emergency Management Planning</h5>
+        <ul style="font-size: 11px; letter-spacing: 0px;">
+          <li>Evacuation Camp Management Plan</li>
+          <li>Contingency Plan</li>
+        </ul>
+        <h5 class="card-title" style="text-align: center; font-weight: 900;">Feasibilities Studies</h5>
+        <ul style="font-size: 11px; letter-spacing: 0px;">
+          <li>Feasible Study</li>
+        </ul>
+        <button class="button1" id="navbtn3" style="font-size: 12px;">Learn More</button>
+      </div>
+    </div>
+  </div>
+</section>
+
+<script src="main.js"></script>
+
+<!-- END OF CHOOSING SERVICES CODE -->
+
+
+<!-- FIRST SERVICE CODE -->
+<section id="fp" class="fp">
+  <h3>Environmental Planning Services</h3>
+<table class="t" style="width: 100%;" >
+  <tr>
+  <th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/zt7pNmDnF98?si=h95Caf-ncDgblPOZ', 'fp', 'embedFrame1')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'fp', 'embedFrame1')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'fp', 'embedFrame1')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'fp', 'embedFrame1')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'fp', 'embedFrame1')">Climate and Disaster Risk Assessment</a></th>
+</tr>
+</table>
+<br>
+<div id="embedContainer1" class="center-container">
+  <div class="iframe-container" style="height: 450px"> 
+    <iframe id="embedFrame1" width="100%" height="100%" src="https://www.youtube.com/embed/zt7pNmDnF98?si=h95Caf-ncDgblPOZ" title="YouTube video player" frameborder="0"
+       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" 
+       allowfullscreen></iframe>
+  </div>
+  <p class="mx-auto p-2" style="width: 200px;">Additonal INFO</p>
+</section>
+<!-- END OF FIRST SERVICE  -->
+
+<!-- SECOND SERVICE CODE  -->
+<section id="sp" class="sp">
+  <h3>Land Use Planning</h3>
+<table class="t" style="width: 100%;">
+<tr>
+  <th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/zt7pNmDnF98?si=h95Caf-ncDgblPOZ', 'sp', 'embedFrame2')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'sp', 'embedFrame2')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'sp', 'embedFrame2')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'sp', 'embedFrame2')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'sp', 'embedFrame2')">Climate and Disaster Risk Assessment</a></th>
+</tr>
+</table>
+<br>
+<div id="embedContainer2" class="center-container">
+  <div class="iframe-container" style="height: 450px"> 
+  <iframe id="embedFrame2" width="100%" height="100%" src="https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6" title="YouTube video player" frameborder="0"
+     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen></iframe>
+  </div>
+</section>
+<!-- END OF SECOND PAGE  -->
+
+<!-- THIRD SERVICE PAGE  -->
+<section id="lup" class="lup">
+  <h3>Community Development Planning</h3>
+<table class="t" style="width: 100%;">
+<tr>
+  <th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/zt7pNmDnF98?si=h95Caf-ncDgblPOZ', 'tp', 'embedFrame3')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'tp', 'embedFrame3')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'tp', 'embedFrame3')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'tp', 'embedFrame3')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'tp', 'embedFrame3')">Climate and Disaster Risk Assessment</a></th>
+</tr>
+</table>
+<br>
+<div id="embedContainer3" class="center-container">
+  <div class="iframe-container" style="height: 450px"> 
+  <iframe id="embedFrame3" width="100%" height="100%" src="https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6" title="YouTube video player" frameborder="0"
+     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen></iframe>
+  </div>
+</section>
+<!-- END OF THIRD PAGE  -->
+
+<!-- FOURTH SERVICE PAGE  -->
+<section id="fthp" class="fthp">
+  <h3>Emergency Management Planning</h3>
+<table class="t" style="width: 100%;">
+<tr>
+  <th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/zt7pNmDnF98?si=h95Caf-ncDgblPOZ', 'fthp', 'embedFrame1')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'fthp', 'embedFrame1')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'fthp', 'embedFrame1')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'fthp', 'embedFrame1')">Climate and Disaster Risk Assessment</a></th>
+<th><a class="button" href="javascript:void(0);" onclick="changeEmbed('https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6', 'fthp', 'embedFrame1')">Climate and Disaster Risk Assessment</a></th>
+</tr>
+</table>
+<br>
+<div id="embedContainer4" class="center-container">
+  <div class="iframe-container" style="height: 450px"> 
+  <iframe id="embedFrame4" width="100%" height="100%" src="https://www.youtube.com/embed/q9luhiQdXLA?si=ZxA1nKcESigpzbH6" title="YouTube video player" frameborder="0"
+     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen></iframe>
+  </div>
+</section>
+<!-- END OF FOURTH PAGE  -->
+
+<!-- LAST PAGE  -->
+<section id="ending" class="ending">
+  <div>
+    <h2>A GOAL WITHOUT<br><u> A PLAN</u><br> IS JUST A WISH</h2>
+    <button class="button3" id="pwu">Plan With Us!</button>
+  </div>  
+</section>
+
+
+
+</body>
+</html>
