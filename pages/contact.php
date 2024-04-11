ef17c4a8e64c69b5b8ce64ac218b19ff5bb27ea4
rainagemanel <rainagemanel2001@gmail.com>
2024-04-11 16:41:59 +0800

    th

--- pages/contact.php
+++ pages/contact.php
@@ -4,42 +4,60 @@
 </body>
 </nav>
 
-<section class="content">
-
-<!-- Default box -->
-<div class="card2">
-  <div class="card-body row">
-    <div class="col-5 text-center d-flex align-items-center justify-content-center">
-      <div class="">
-        <h2>Admin<strong>LTE</strong></h2>
-        <p class="lead mb-5">123 Testing Ave, Testtown, 9876 NA<br>
-          Phone: +1 234 56789012
-        </p>
-      </div>
-    </div>
-    <div class="col-7">
-      <div class="form-group">
-        <label for="inputName">Name</label>
-        <input type="text" id="inputName" class="form-control" />
-      </div>
-      <div class="form-group">
-        <label for="inputEmail">E-Mail</label>
-        <input type="email" id="inputEmail" class="form-control" />
-      </div>
-      <div class="form-group">
-        <label for="inputSubject">Subject</label>
-        <input type="text" id="inputSubject" class="form-control" />
-      </div>
-      <div class="form-group">
-        <label for="inputMessage">Message</label>
-        <textarea id="inputMessage" class="form-control" rows="4"></textarea>
-      </div>
-      <div class="form-group">
-        <input type="submit" class="btn btn-primary" value="Send message">
+<body><section class="content">
+  <br>
+<h2 class="text-center mt-3" style="font-size: 44px; font-family: 'Audiowide', cursive !important;">Connect to us</h2>
+  <div class="card2-container">
+    <div class="card2">
+      <div class="card-body row">
+        <div class="col-5 text-center d-flex align-items-center justify-content-center">
+          <div class="">
+            <h2 style="font-size: 30px; font-family: 'Audiowide', cursive !important;">Geoinfometrics</h2>
+            <p class="lead mb-5">Anest Tower GF, 10411 Lopez Avenue <br>Los Baños, Philippines<br>
+              Phone: +1 234 56789012
+            </p>
+          </div>
+        </div>
+        <div class="col-7">
+          <div class="form-group">
+            <label for="inputName">Name</label>
+            <input type="text" id="inputName" class="form-control" />
+          </div>
+          <div class="form-group">
+            <label for="inputEmail">E-Mail</label>
+            <input type="email" id="inputEmail" class="form-control" />
+          </div>
+          <div class="form-group">
+            <label for="inputSubject">Subject</label>
+            <input type="text" id="inputSubject" class="form-control" />
+          </div>
+          <div class="form-group">
+            <label for="inputMessage">Message</label>
+            <textarea id="inputMessage" class="form-control" rows="4"></textarea>
+          </div>
+          <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Send message">
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>
-
 </section>
-<!-- /.content -->
+<div class="col-md-12 text-center py-4">
+        <h2 class="text-center mt-3" style="font-size: 44px; font-family: 'Audiowide', cursive !important;">Where to find us?</h2>
+      </div>
+<section class="content">
+  <!-- Default box -->
+  <div class="card2-container">
+    <div class="card2">
+      <div class="card-body row align-items-center">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241.77512326105725!2d121.24306493905702!3d14.171218832598154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd60b9b62ace39%3A0x9aa2934c8f26810b!2sAnest%20Tower!5e0!3m2!1sen!2sph!4v1712822306673!5m2!1sen!2sph" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php include 'footer.php'; ?>
+</body>
+</html>
